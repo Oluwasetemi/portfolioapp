f163c1c69ef9a98f0fa3e0dadfb2a62022a1d3ed
AbidemiT <abidemis01@gmail.com>
2018-09-27 15:19:10 +0100

    like

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
     <div class="contact main">
                 <div class="box">
         <h2>GET IN TOUCH</h2>
-        <form name="contact" netlify>
+        <form name="contact" method="POST" netlify>
             <div class="inputBox">
                 <input type="text" name="name" required="">
                 <label for="">Name</label>
