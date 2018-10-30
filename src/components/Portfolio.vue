db9a31d025fc1c194278b624ea84405cd977762a
AbidemiT <abidemis@gmail.com>
2018-10-30 11:45:23 +0100

    modified Portfolio.vue

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -2,7 +2,7 @@
   <div class="portfolio main">
     
     <main>
-      <div >
+      <div class="div">
         <img class="img" src="@/assets/i.jpg" alt="Myimage">
       </div>
         
@@ -31,11 +31,21 @@ export default {
   width: 240px;
   border-radius: 50%;
 }
+.div {
+  margin-bottom: 10px;
+}
 main {
   display: grid;
-  grid-template-rows: 3fr .5fr 1fr ;
+  grid-template-rows: 3fr 0.5fr 1fr;
   text-align: center;
   grid-row-gap: 5px;
 }
-
+@media (max-width: 500px) {
+  main {
+    display: grid;
+    grid-template-rows: 2.2fr 0.4fr 0.6fr;
+    text-align: center;
+    grid-gap: 5px;
+  }
+}
 </style>
